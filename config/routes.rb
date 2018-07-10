91d1bf7fb48b154ab118a18d0febfef75a9ff35d
EC2 Default User <ec2-user@ip-172-31-17-227.eu-west-1.compute.internal>
2018-07-10 09:20:30 +0000

    removed json from controller meeting

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
   get 'heats/update'
   root "meetings#index"
   resources :meetings
-  resources :heats
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
